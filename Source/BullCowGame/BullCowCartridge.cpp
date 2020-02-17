aa4c4f4673f10ade6c8926587435d1944145a7b5
Roman <roman.liewehr@hotmail.com>
2020-02-17 18:37:51 +0000

    xx

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,30 @@
+
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    
     Super::BeginPlay();
+    PrintLine(TEXT("Bull & Cows are welcoming a new player!\nCome Closer and press TAB"));
+    HiddenWord = TEXT("cake");
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+   // PrintLine(Input);
 
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("THAT'S CORRECT"));
+
+    } else
+    {
+        PrintLine(TEXT("WRONG"));
+    }
+     
 }
