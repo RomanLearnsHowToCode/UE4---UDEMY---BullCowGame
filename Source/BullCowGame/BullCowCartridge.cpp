2669569f4a0bc43cb41e18f5b0341f5f1d804283
Roman <roman.liewehr@hotmail.com>
2020-02-18 14:56:01 +0000

    xxp01

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,6 +1,7 @@
 
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWordList.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
@@ -9,8 +10,14 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
     InitGame();
 
-
+    PrintLine(TEXT("The number of possible words is %i"),Words.Num()); // Debug line    
     PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord); // Debug line
+
+    for (int32 Index = 0; Index != 5; ++Index)
+    {
+        PrintLine(TEXT("%s"), *Words[Index]);
+    }
+    
 
     //SETTING UP THE GAME
     
@@ -45,6 +52,8 @@ void UBullCowCartridge::InitGame() // SetupGame()
     PrintLine(TEXT("You have got %i Lives: "), Lives);
     PrintLine(TEXT("\nPress Enter to continue"));
     PrintLine(TEXT("Character 1 of the hidden word is: %c"), HiddenWord[0]); // print "c"
+
+    //IsIsogram(HiddenWord);
 
     // const TCHAR HW[] = TEXT("plums"); 
     // PrintLine(TEXT("The 4th character of HW is: %c"), HW[3]); // prints "m"
@@ -126,17 +135,20 @@ void UBullCowCartridge::InitGame() // SetupGame()
 
  bool UBullCowCartridge::IsIsogram(FString Word) const // CONST function
  {
-    //  For each letter
-    //  Start at element [0]
-    //  Compare against the next letter.
-    //  Until we reach the end of an array [Word.Len() -1]
-    //  if any letter are same return false
 
     for (int32 Index = 0; Index < Word.Len(); Index++)
     {
-        PrintLine(TEXT("%c"), Word[Index]);
+        for (int32 Comparison =  Index + 1; Comparison < Word.Len(); Comparison++)
+        {
+            if (Word[Index] == Word[Comparison])
+            {
+                return false;
+            }
+            
+        }
+
     }
-    
 
     return true;
- };
+     
+ }
